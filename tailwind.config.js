f03166d67e1400d1619ae9a5d815b527187f2856
administracja-ssuew <administracja@samorzad.ue.wroc.pl>
2026-03-01 16:10:51 +0100

    Dodanie wzorca safelist dla kolorów z Excela

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
     {
       // Ten wzór chroni przed usunięciem WSZYSTKIE najpopularniejsze kolory tła z Tailwinda.
       // Od teraz możesz wpisać w Excelu np. bg-fuchsia-500, bg-lime-500, bg-cyan-600 itd.!
-      pattern: /bg-(slate|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(400|500|600|700)/,
+      pattern: /bg-(slate|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(300|400|500|600|700|800)/,
     }
   ],
 
