5bcdac53cccfa49bc596a69dde2076fab27b2b99
administracja-ssuew <administracja@samorzad.ue.wroc.pl>
2026-03-26 22:33:39 +0100

    Naprawa bezpieczeństwa - logowanie (config.js) 1.1

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const CRW_API_URL = "https://script.google.com/macros/s/AKfycbyRZFBR-7Lo2I-hXnFykVV5Bose6Z4tv7Hp7Si5LGV9lsiVdx8pCIKXBy_Z5eytRHQzGg/exec";
+export const CRW_API_URL = "https://script.google.com/macros/s/AKfycbyRZFBR-7Lo2I-hXnFykVV5Bose6Z4tv7Hp7Si5LGV9lsiVdx8pCIKXBy_Z5eytRHQzGg/exec"
